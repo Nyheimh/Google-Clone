0ebefdddf98348fa9e733fdb7cd15c3febabad42
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-04-04 23:23:34 -0700

    Completed SearchPage

--- noogle/src/components/SearchPage/SearchPage.js
+++ noogle/src/components/SearchPage/SearchPage.js
@@ -1,37 +1,85 @@
-import React from 'react'
-import './SearchPage.css'
-import { useStateValue } from "../../StateProvider"
-import useGoogleSearch from '../../useGoogleSearch';
-import Response from "../../response"
-import { Link } from "react-router-dom"
-import Search from "../Search/Search"
+import React from "react";
+import "./SearchPage.css";
+import { useStateValue } from "../../StateProvider";
+import useGoogleSearch from "../../useGoogleSearch";
+import Response from "../../response";
+import { Link } from "react-router-dom";
+import Search from "../Search/Search";
+import SearchIcon from "@material-ui/icons/Search"
+import DescriptionIcon from "@material-ui/icons/Description"
+import ImageIcon from "@material-ui/icons/Image"
+import LocalOfferIcon from "@material-ui/icons/LocalOffer"
+import RoomIcon from "@material-ui/icons/Room"
+import MoreVertIcon from "@material-ui/icons/MoreVert"
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
+  
   //LIVE API CALL
-  // const { data } = useGoogleSearch(term); 
+  // const { data } = useGoogleSearch(term);
   const data = Response;
 
-
-  console.log(data)
+  console.log(data);
   return (
-    <div className='searchPage'>
-      <div className='searchPage__header'>
+    <div className="searchPage">
+      <div className="searchPage__header">
         <Link to="/">
-        <img
-          className="searchPage__logo"
-          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
-          alt=""
-        />
+          <img
+            className="searchPage__logo"
+            src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+            alt=""
+          />
         </Link>
-        </div>
+      </div>
 
-      <div className='searchPage__headerBody'>
+      <div className="searchPage__headerBody">
         <Search hideButtons />
 
+        <div className="searchPage__options">
+
+        </div>
+        <div className="searchPage__optionsLeft">
+
+        </div>
+        <div className="searchPage__option">
+          <SearchIcon />
+          <Link to="/all">All</Link>
+          <DescriptionIcon />
+          <Link to="/news">News</Link>
+        </div>
+        <div className="searchPage__option">
+          <ImageIcon />
+          <Link to="/images">Images</Link>
+        </div>
+        <div className="searchPage__option">
+          <LocalOfferIcon />
+          <Link to="/shopping">Shopping</Link>
+        </div>
+        <div className="searchPage__option">
+          <RoomIcon />
+          <Link to="/maps">Maps</Link>
+        </div>
+        <div className="searchPage__option">
+          <MoreVertIcon />
+          <Link to="/more">More</Link>
+        </div>
+        <div className="searchPage__optionsRight">
+          <div className="searchPage__option">
+          <Link to="/settings">Settings</Link>
+
+          </div>
+          <div className="searchPage__option">
+          <Link to="/tools">Tools</Link>
+
+        </div>
+
+        </div>
+        <div className="searchPage__results">
+
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default SearchPage
+export default SearchPage;
