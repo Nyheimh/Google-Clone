e45e439c54289d0442fbc1efe3e68cb81bbb752f
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-04-04 23:52:10 -0700

    need styling

--- noogle/src/components/SearchPage/SearchPage.js
+++ noogle/src/components/SearchPage/SearchPage.js
@@ -5,19 +5,19 @@ import useGoogleSearch from "../../useGoogleSearch";
 import Response from "../../response";
 import { Link } from "react-router-dom";
 import Search from "../Search/Search";
-import SearchIcon from "@material-ui/icons/Search"
-import DescriptionIcon from "@material-ui/icons/Description"
-import ImageIcon from "@material-ui/icons/Image"
-import LocalOfferIcon from "@material-ui/icons/LocalOffer"
-import RoomIcon from "@material-ui/icons/Room"
-import MoreVertIcon from "@material-ui/icons/MoreVert"
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
-  
+
   //LIVE API CALL
-  // const { data } = useGoogleSearch(term);
-  const data = Response;
+  const { data } = useGoogleSearch(term);
+  // const data = Response;
 
   console.log(data);
   return (
@@ -35,12 +35,8 @@ function SearchPage() {
       <div className="searchPage__headerBody">
         <Search hideButtons />
 
-        <div className="searchPage__options">
-
-        </div>
-        <div className="searchPage__optionsLeft">
-
-        </div>
+        <div className="searchPage__options"></div>
+        <div className="searchPage__optionsLeft"></div>
         <div className="searchPage__option">
           <SearchIcon />
           <Link to="/all">All</Link>
@@ -65,19 +61,35 @@ function SearchPage() {
         </div>
         <div className="searchPage__optionsRight">
           <div className="searchPage__option">
-          <Link to="/settings">Settings</Link>
-
+            <Link to="/settings">Settings</Link>
           </div>
           <div className="searchPage__option">
-          <Link to="/tools">Tools</Link>
-
-        </div>
-
-        </div>
-        <div className="searchPage__results">
-
+            <Link to="/tools">Tools</Link>
+          </div>
         </div>
       </div>
+      {term && (
+        <div className="searchPage__results">
+          <p className="searchPage__resultCount">
+            About {data?.searchInformation.formattedTotalResults}results (
+            {data?.searchInformation.formattedSearchTime} seconds) for {term}
+          </p>
+          {data?.items.map((item) => (
+            <div className="searchPage__result">
+              {item.pagemap?.cse_image?.length > 0 item.pagemap?.cse_image[0]?.src && (
+              
+              )}
+
+              {item.displayLink}
+              <a href={item.link}>{items.displayLink}</a>
+              <a className="searchPage__resultTitle" href={item.link}>
+                <h2>{item.title}</h2>
+              </a>
+              <p className="searchPage__resultSnippet">{item.snippet}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- noogle/src/screens/Home/Home.js
+++ noogle/src/screens/Home/Home.js
@@ -26,7 +26,9 @@ function Home() {
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           alt=""
         />
-        <Search hideButtons />
+        <div className="home__inputContainer">
+        <Search />
+        </div>
       </div>
     </div>
   );
