33f4c2348c9344fdedb25dcf7eacf1179a5ee0e1
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-04-04 23:02:31 -0700

    searchpage

--- noogle/src/components/SearchPage/SearchPage.js
+++ noogle/src/components/SearchPage/SearchPage.js
@@ -2,18 +2,32 @@ import React from 'react'
 import './SearchPage.css'
 import { useStateValue } from "../../StateProvider"
 import useGoogleSearch from '../../useGoogleSearch';
+import Response from "../../response"
+import { Link } from "react-router-dom"
+import Search from "../Search/Search"
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
-  const { data } = useGoogleSearch(term);
+  //LIVE API CALL
+  // const { data } = useGoogleSearch(term); 
+  const data = Response;
+
+
   console.log(data)
   return (
     <div className='searchPage'>
       <div className='searchPage__header'>
-        <h1>{term}</h1>
-      </div>
+        <Link to="/">
+        <img
+          className="searchPage__logo"
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png"
+          alt=""
+        />
+        </Link>
+        </div>
 
-      <div className='searchPage__results'>
+      <div className='searchPage__headerBody'>
+        <Search hideButtons />
 
       </div>
     </div>
