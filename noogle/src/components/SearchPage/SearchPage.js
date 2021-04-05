1e11d17cf9a5f23621211f79a3ba7cdfb89cc630
Nyheim Hunter <nyheimhunter@Nyheims-Air.attlocal.net>
2021-04-05 08:41:16 -0700

    Working on Searchpage CSS

--- noogle/src/components/SearchPage/SearchPage.js
+++ noogle/src/components/SearchPage/SearchPage.js
@@ -36,31 +36,29 @@ function SearchPage() {
         </div>
       </div>
       <div className="searchPage__options">
-        
         <div className="searchPage__optionsLeft">
-
-        <div className="searchPage__option">
-          <SearchIcon />
-          <Link to="/all">All</Link>
-          <DescriptionIcon />
-          <Link to="/news">News</Link>
-        </div>
-        <div className="searchPage__option">
-          <ImageIcon />
-          <Link to="/images">Images</Link>
-        </div>
-        <div className="searchPage__option">
-          <LocalOfferIcon />
-          <Link to="/shopping">Shopping</Link>
-        </div>
-        <div className="searchPage__option">
-          <RoomIcon />
-          <Link to="/maps">Maps</Link>
-        </div>
-        <div className="searchPage__option">
-          <MoreVertIcon />
-          <Link to="/more">More</Link>
-        </div>
+          <div className="searchPage__option">
+            <SearchIcon />
+            <Link to="/all">All</Link>
+            <DescriptionIcon />
+            <Link to="/news">News</Link>
+          </div>
+          <div className="searchPage__option">
+            <ImageIcon />
+            <Link to="/images">Images</Link>
+          </div>
+          <div className="searchPage__option">
+            <LocalOfferIcon />
+            <Link to="/shopping">Shopping</Link>
+          </div>
+          <div className="searchPage__option">
+            <RoomIcon />
+            <Link to="/maps">Maps</Link>
+          </div>
+          <div className="searchPage__option">
+            <MoreVertIcon />
+            <Link to="/more">More</Link>
+          </div>
         </div>
         <div className="searchPage__optionsRight">
           <div className="searchPage__option">
@@ -71,41 +69,42 @@ function SearchPage() {
           </div>
         </div>
       </div>
-    </div>
-    
-      {term && (
-        <div
-      className="searchPage__results">
-      <p
-        className="searchPage__resultCount">
-            About {data?.searchInformation.formattedTotalResults} results 
-            ({data?.searchInformation.formattedSearchTime} seconds) for {term}
+      {/* </div> */}
+
+      {true && (
+        <div className="searchPage__results">
+          <p className="searchPage__resultCount">
+            About {data?.searchInformation.formattedTotalResults} results (
+            {data?.searchInformation.formattedSearchTime} seconds) for {term}
           </p>
-      {data?.items.map(item => (
-        <div className="searchPage__result">
-          <a href={item.link}>{items.displayLink}
-            {item.pagemap?.cse_image?.length > 0 item.pagemap?.cse_image[0]?.src && (
-              <img
-              className="searchPage__resultImage"
-              src={
-                item.pagemap?.cse_image?.length > 0 &&
-                item.pagemap?.cse_image[0]?.src
-              }
-              alt=""
-              />
-                )}
+
+          {data?.items.map((item) => (
+            <div className="searchPage__result">
+              <a className="searchPage__resultLink" href={item.link}>
+                {item.pagemap?.cse_image?.length > 0 &&
+                  item.pagemap?.cse_image[0]?.src && (
+                    // {items.displayLink}
+                    <img
+                      className="searchPage__resultImage"
+                      src={
+                        item.pagemap?.cse_image?.length > 0 &&
+                        item.pagemap?.cse_image[0]?.src
+                      }
+                      alt=""
+                    />
+                  )}
+
                 {item.displayLink}
-          </a>
-          <a className="searchPage__resultTitle" href={item.link}>
-            <h2>{item.title}</h2>
-          </a>
-          <p className="searchPage__resultSnippet">{item.snippet}</p>
-              
-        </div>
-      ))}
+              </a>
+              <a className="searchPage__resultTitle" href={item.link}>
+                <h2>{item.title}</h2>
+              </a>
+              <p className="searchPage__resultSnippet">{item.snippet}</p>
             </div>
-      )}
+          ))}
         </div>
+      )}
+    </div>
   );
 }
 
